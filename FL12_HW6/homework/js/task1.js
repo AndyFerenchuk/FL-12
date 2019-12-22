49099c20f69b1b05cdcae91abb9643f0db4ec78c
AndyFerenchuk <ferenchuk.a@hotmail.com>
2019-12-22 22:44:44 +0200

    task 1 app working

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,30 @@
-// Your code goes here
+let x1, x2;
+let a = prompt("enter a",);
+a = parseFloat(a);
+if (typeof a !== "number" || a === 0) {
+    console.log('invalid input')
+} else {
+    let b = prompt("enter b",);
+    b = parseFloat(b);
+    if (typeof b !== "number") {
+        console.log('invalid input')
+    } else {
+        let c = prompt("enter c",);
+        c = parseFloat(c);
+        if (typeof c !== "number") {
+            console.log('invalid input')
+        } else {
+            let d = b ** 2 - 4 * a * c;
+            console.log('d = ' + ' ' + d);
+            if (d < 0) {
+                console.log('no solution');
+            } else {
+                x1 = (-b + d ** (1 / 2)) / (2 * a);
+                x2 = (-b - d ** (1 / 2)) / (2 * a);
+                console.log('x1 = ' + ' ' + x1);
+                console.log('x2 = ' + ' ' + x2);
+            }
+        }
+    }
+}
+
