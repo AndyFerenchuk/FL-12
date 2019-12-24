f009ba2ac5e440a2d0ff836dbe11df2f9478df11
AndyFerenchuk <ferenchuk.a@hotmail.com>
2019-12-24 15:01:06 +0200

    minor changes for task2

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,6 +1,6 @@
-let a = prompt('enter value of a');
-let b = prompt('enter value of b');
-let c = prompt('enter value of c');
+let a = + prompt('enter value of a');
+let b = + prompt('enter value of b');
+let c = + prompt('enter value of c');
 if ((typeof a === "string" && a.length === 0 || typeof a === "undefined") || (typeof b === "string" && b.length === 0 || typeof b === "undefined") || (typeof c === "string" && c.length === 0 || typeof c === "undefined")) {
     alert('input values should be ONLY numbers');
 } else if (Number(a.valueOf()) === 0 || Number(b.valueOf()) === 0 || Number(c.valueOf()) === 0) {
