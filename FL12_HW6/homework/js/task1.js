0d3ac523bd881a145175b4de75f16c6010600893
AndyFerenchuk <ferenchuk.a@hotmail.com>
2019-12-24 12:39:34 +0200

    task1 is lint free now

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,26 +1,26 @@
 let x1, x2;
-let a = prompt("enter a",);
+let a = prompt("enter a");
 a = parseFloat(a);
 if (isNaN(a) || a === 0) {
     console.log('invalid input')
 } else {
-    let b = prompt("enter b",);
+    let b = prompt("enter b");
     b = parseFloat(b);
     if (isNaN(b)) {
         console.log('invalid input')
     } else {
-        let c = prompt("enter c",);
+        let c = prompt("enter c");
         c = parseFloat(c);
         if (isNaN(c)) {
             console.log('invalid input')
         } else {
-            let d = b ** 2 - 4 * a * c;
+            let d = Math.pow(b, 2)  - 4 * a * c;
             console.log('d = ' + ' ' + d);
             if (d < 0) {
                 console.log('no solution');
             } else {
-                x1 = (-b + d ** (1 / 2)) / (2 * a);
-                x2 = (-b - d ** (1 / 2)) / (2 * a);
+                x1 = (-b + Math.sqrt(d)) / (2 * a);
+                x2 = (-b - Math.sqrt(d)) / (2 * a);
                 console.log('x1 = ' + ' ' + x1);
                 console.log('x2 = ' + ' ' + x2);
             }
